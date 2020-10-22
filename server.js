697c851c7cb44e84e6d42f5ce7c7f8a103c2a03d
JenIglar <jeniglar@gmail.com>
2020-10-22 14:56:14 -0400

    Added front end routes and required morgan

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
@@ -11,6 +15,14 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(_dirname, "public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(_dirname, "public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(_dirname, "public/stats.html"));
 });
 
 app.listen(PORT, () => {
