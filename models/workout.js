a1d8457bfcbfd5df9a2b4dca6b8a11a571920ca9
JenIglar <jeniglar@gmail.com>
2020-10-22 13:22:13 -0400

    Added index.js require and export

--- models/workout.js
+++ models/workout.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+// Add additional criteria to exercises
+///////////////////////////////////
     day: Date,
     exercises: Array
 });
